33adc417b786b2447d3862d55c95e6b02557acb7
Chrisbal <christianbalinda723@gmail.com>
2023-01-12 12:49:04 +0200

    download mp4 file

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 #! python3
 
 import logging
+import pytube
 from pytube import YouTube
 import sys
 import os
 
 logging.basicConfig(level=logging.DEBUG, format='%(levelname)s - %(message)s - %(asctime)s')
-logging.disable()
+# logging.disable()
 
 link = ''
 
@@ -15,5 +16,8 @@ os.chdir(r'C:\Users\Admin\Downloads')
 if len(sys.argv) > 1:
     link = sys.argv[1]
     yt = YouTube(link)
-    yt.streams.first().download()
+    download_check = yt.streams.filter(progressive=True, file_extension='mp4')\
+        .order_by('resolution').desc().first().download()
+    if download_check:
+        print('Download completed', download_check)
 
