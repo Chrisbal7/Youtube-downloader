d410a4ef0ab87bf18538d2966089f4e1288613bd
Chrisbal <christianbalinda723@gmail.com>
2023-01-12 14:47:03 +0200

    Implement % downloaded

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
 #! python3
 
 import logging
-import pytube
 from pytube import YouTube
-import sys
+from pytube.cli import on_progress
 import os
+import time
 
 logging.basicConfig(level=logging.DEBUG, format='%(levelname)s - %(message)s - %(asctime)s')
-# logging.disable()
-
-link = ''
+logging.disable()
 
 os.chdir(r'C:\Users\Admin\Downloads')
+link = input('Paste the link here, Click ENTER to download\n')
 
-if len(sys.argv) > 1:
-    link = sys.argv[1]
-    yt = YouTube(link)
-    download_check = yt.streams.filter(progressive=True, file_extension='mp4')\
-        .order_by('resolution').desc().first().download()
-    if download_check:
-        print('Download completed', download_check)
 
+def percent(tem, total):
+    percentage = ((float(tem) / float(total)) * 100)
+    return int(percentage)
+
+
+perc = ''
+
+
+def progress_func(stream, _chunk, bytes_remaining):
+    global perc
+    print('\b' * len(perc), end='', flush=True)
+    size = stream.filesize
+    downloaded = size - bytes_remaining
+    print(perc, end='')
+    perc = str(percent(downloaded, size)) + ' % '
+
+
+yt = YouTube(link, on_progress_callback=progress_func)
+download_check = yt.streams.filter(progressive=True, file_extension='mp4')\
+    .order_by('resolution').desc().first().download()
+if download_check:
+    print('Download completed', download_check)
